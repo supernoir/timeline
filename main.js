7a3f1fcd2407cbeafa81770690efb55ef29c4a6f
Conny <kawohl.bentodesign@gmail.com>
2016-10-21 14:44:59 +0200

    Adds Button that let's you add to current state

--- main.js
+++ main.js
@@ -9,8 +9,32 @@ class Timeline extends React.Component{
             <div>
             <h1>Hello World!</h1>
             <ul><FirstNode /><SingleNode /><LastNode /></ul>
+            <NodeAdder />
             </div>);
 	}
+}
+
+class NodeAdder extends React.Component{
+      constructor(){
+            super();
+            this.state = {
+                  added: 1
+            }
+            this.addNodeEvent = this.addNodeEvent.bind(this);
+      }
+
+addNodeEvent(){
+            let x = this.state.added;
+            x = x+1;
+            this.setState({added: x});
+            
+      }
+
+      render(){
+            const added = this.state.added;
+      return <div><button onClick={this.addNodeEvent}>Add Node</button><br />{added}</div>
+      }
+      
 }
 
 let SingleNode = () => {
@@ -23,6 +47,10 @@ let FirstNode = () => {
 
 let LastNode = () => {
       return <li>I am the very last node</li>
+}
+
+let AddNode = () => {
+      return <li>I am an added node!</li>
 }
 
 ReactDOM.render(<Timeline />, document.getElementById('app'))
